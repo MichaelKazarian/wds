e92bd5078e6d00f9cd77ab1102776c5d3ab1d244
kazarian <michael.kazarian@gmail.com>
2020-12-01 12:12:33 +0200

    .dir-locals was added

---
+++ .dir-locals.el
@@ -0,0 +1,18 @@
+((nil . ((eval . (git-gutter-mode))
+         ))
+ ;; (emacs-lisp-mode . ((indicate-empty-lines . t)
+ ;;                     ))
+ )
+;;; .dir-locals.el --- 
+
+;; Copyright (C) Michael Kazarian
+;;
+;; Author: Michael Kazarian <michael.kazarian@gmail.com>
+;; Keywords: 
+;; Requirements: 
+;; Status: not intended to be distributed yet
+
+
+
+
+;;; .dir-locals.el ends here
